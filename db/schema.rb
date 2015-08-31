0570acbec26a1697c0506504ae78c8f53ac4a112
mauricioedu <olive.y.ra@hotmail.com>
2015-08-31 19:01:01 -0300

    add a associacao entre post e user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828222957) do
+ActiveRecord::Schema.define(version: 20150831215704) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 20150828222957) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
